0b3806e005fd17a05350af63f414275def0d8b7f
Amirsepehr <amirsepehr.shamloo.1996@gmail.com>
2026-08-17 10:42:45 -0700

    first commit: Observe data, did some preprocessing, splitting, scaling, and saving scaler

---
+++ src/data_prep.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import joblib
+
+
+
+data = pd.read_csv(r"E:\mini-project01\data\creditcard.csv")
+print(data.shape)
+
+# Quick Show
+data.info()
+print(data.head())
+print(data.describe())
+print(data.isnull().sum())
+print(data['Class'].value_counts())
+print(data['Class'].value_counts(normalize=True) * 100)
+print(data.duplicated().sum())
+
+# Preprocessing
+data = data.drop_duplicates()
+print(data['Class'].value_counts())
+print(data['Class'].value_counts(normalize=True) * 100)
+
+data['Hour'] = (data['Time'] % (24 * 3600)) / 3600
+data = data.drop('Time', axis=1)
+print(data.describe())
+
+X = data.drop(columns=['Class'])
+y = data['Class']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+print(y_train.value_counts(normalize=True))
+print(y_test.value_counts(normalize=True))
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
+joblib.dump(scaler, r"E:\mini-project01\models\scaler.pkl")
